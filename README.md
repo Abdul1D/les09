4d26c92223f219fefe830dc25a7b81e0a799eb45
Appie <37385@ma-web.nl>
2023-10-11 09:47:09 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# les09
+function HaikuX(){
+     console.log("haiku by:","AUTHOR");
+     console.log("REGEL1");
+     console.log("REGEL2");
+     console.log("REGEL3");
+}
+
+function Haiku1(){
+    console.log("haiku by:","Docent");
+    console.log("Panda in the mist");
+    console.log("Gently chewing on bamboo");
+    console.log("Perfect breakfast time");
+}
+
+
+function Haiku1(){
+    console.log("haiku by:","Docent");
+    console.log("Panda in the mist");
+    console.log("Gently chewing on bamboo");
+    console.log("Perfect breakfast time");
+}
---
+++ les09.js
@@ -0,0 +1,22 @@
+function HaikuX(){
+    console.log("haiku by:","saad");
+    console.log("REGEL1");
+    console.log("REGEL2");
+    console.log("REGEL3");
+}
+function HaikuX(){
+    console.log("haiku by:","Abdulpapi");
+    console.log("I don't understand. You love it when I do that Wait, no. That's Stephen.");
+    console.log(" Frantically hiding Porno and Mapplethorpe prints-- Mom is on her way.");
+    console.log("Remember when I Said I disliked oral sex I meant just with you.");
+}
+function HaikuX(){
+    console.log("haiku by:","Dikian");
+    console.log("REGEL1");
+    console.log("REGEL2");
+    console.log("REGEL3");
+}
+
+Haiku1()
+Haiku2()
+Haiku3()
